5314c0bd78d76102d5ae3d580552d5e9600649fe
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-27 21:08:51 +0300

    fix: tags

--- src/components/tags/tags.tsx
+++ src/components/tags/tags.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Tag from '../tag';
@@ -8,9 +8,16 @@ import style from './tags.module.css';
 type TagsPropsType = { tags: Tag[] | undefined; }
 
 export default function Tags({ tags }: TagsPropsType) {
+  const processedTags = tags || [];
+  const renderedTags = useMemo(() => processedTags.map((tag) => (
+    <Tag
+      key={tag.id || uuidv4()}
+      tag={tag}
+    />
+  )), [processedTags]);
   return (
     <ul className={style.tags}>
-      {tags?.map((tag) => <Tag key={uuidv4()} tag={tag} />)}
+      {renderedTags}
     </ul>
   );
 }
