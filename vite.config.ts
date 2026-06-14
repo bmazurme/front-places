9fbd081bdffd9cca29ce1caf71d56fb64097d3d3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-06-14 12:39:13 +0300

    fix: libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
